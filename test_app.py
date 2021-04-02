cbb68092467422f22af3a95eef979d540c23d453
Andreas Varotsis <andrea.varotsis@gmail.com>
2021-04-02 10:25:37 +0100

    initial upload and commit
    basic functionality

---
+++ test_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+from fastai.vision.all import *
+from fastai.vision.widgets import *
+
+
+
+learn_inf = load_learner('export.pkl')
+
+result= "Awaiting Upload"
+result_detail = ""
+
+results_dict = {"butterfly":["Butterfly Knife","A balisong, also known as a fan knife, butterfly knife or Batangas knife, is a type of folding pocketknife that originated in the Philippines. Its distinct features are two handles counter-rotating around the tang such that, when closed, the blade is concealed within grooves in the handles."],
+                "pocket":["Pocket Knife", "A pocketknife is a foldable knife with one or more blades that fit inside the handle that can still fit in a pocket."],
+               "machete":["Machete", "A machete is a broad blade used either as an agricultural implement similar to an axe, or in combat like a long-bladed knife. The blade is typically 30 to 45 centimetres (12 to 18 in) long and usually under 3 millimetres (0.12 in) thick."],
+               "bayonet":["Bayonet or Combat Knife", "A combat knife is a fighting knife designed solely for military use and primarily intended for hand-to-hand or close combat fighting.[1][2][3]"],
+               "kitchen":["Kitchen knife", "A household knife intended for cooking"]
+}
+st.title('Knife Classifier')
+st.write("Upload your knife picture below and click confirm to begin classification")
+
+knife_pic = st.file_uploader("Your Knife Picture", type=["png","jpg","bmp", "tiff","gif","eps","raw","jpeg",], accept_multiple_files=False, key="knife_pic")
+
+if knife_pic is not None:
+    pred, pred_idx, probs = learn_inf.predict(knife_pic)
+    result = "You uploaded a " + results_dict[pred][0]
+    result_detail = results_dict[pred][1]
+
+if knife_pic is not None:
+    st.image(knife_pic, width=150)
+
+st.write(result)
+st.write(result_detail)
